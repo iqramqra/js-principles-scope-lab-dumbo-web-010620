f387b35465f93b8c4f926836cfe6cf9ffe7285c4
Iqra Masroor <masrooriqra@gmail.com>
2020-02-16 18:41:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Write your solution in this file!
 var customerName = 'bob';
+const leastFavoriteCustomer = 'some initial value'
+var bestCustomer;
 
 function upperCaseCustomerName(){
   customerName = customerName.toUpperCase()
